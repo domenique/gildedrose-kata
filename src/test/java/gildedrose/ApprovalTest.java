02df9674ba81cc9bcaee76045433827d11e05ed3
Doménique Tilleuil <Domenique.Tilleuil@vlaanderen.be>
2021-10-28 13:20:04 +0200

    cleanup

--- src/test/java/gildedrose/ApprovalTest.java
+++ src/test/java/gildedrose/ApprovalTest.java
@@ -1,20 +1,22 @@
 package gildedrose;
 
 import org.approvaltests.combinations.CombinationApprovals;
+import org.approvaltests.reporters.UseReporter;
 import org.junit.jupiter.api.Test;
 
+@UseReporter(KsDiffReporter.class)
 class ApprovalTest {
 
     @Test
     void approvalTest() {
         String[] names = {"foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"};
-        Integer[] sellIns = {10,0};
+        Integer[] sellIns = {10, 0};
         Integer[] qualities = {20};
         CombinationApprovals.verifyAllCombinations(this::runIt, names, sellIns, qualities);
     }
 
     private String runIt(String name, int sellIn, int quality) {
-        var items = new Item[]{new Item(name, sellIn, quality )};
+        var items = new Item[]{new Item(name, sellIn, quality)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         return app.items[0].toString();
--- src/test/java/gildedrose/PackageSettings.java
+++
@@ -1,8 +0,0 @@
-package gildedrose;
-
-import org.approvaltests.reporters.GenericDiffReporter;
-
-public class PackageSettings {
-
-    public static GenericDiffReporter UseReporter = new KsDiffReporter();
-}
