d599a89da06c8bfc59367cdff9b8350d1d147c96
Doménique Tilleuil <domenique@tripled.io>
2022-12-01 20:56:15 +0100

    bumping versions

--- src/test/java/gildedrose/ApprovalTest.java
+++ src/test/java/gildedrose/ApprovalTest.java
@@ -1,17 +1,22 @@
 package gildedrose;
 
 import org.approvaltests.combinations.CombinationApprovals;
+import org.approvaltests.reporters.DiffReporter;
 import org.approvaltests.reporters.UseReporter;
+import org.approvaltests.reporters.macosx.KaleidoscopeDiffReporter;
 import org.junit.jupiter.api.Test;
 
-@UseReporter(KsDiffReporter.class)
+import javax.swing.*;
+
+
+@UseReporter(KaleidoscopeDiffReporter.class)
 class ApprovalTest {
 
     @Test
     void approvalTest() {
         String[] names = {"foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"};
-        Integer[] sellIns = {10, 0};
-        Integer[] qualities = {20};
+        Integer[] sellIns = {0, 1, 2, 3, 4, 10};
+        Integer[] qualities = {0, 1, 2, 3, 10, 20};
         CombinationApprovals.verifyAllCombinations(this::runIt, names, sellIns, qualities);
     }
 
