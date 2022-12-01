94713858dd753cfee78e46d71b8db2c6ca4cc3bb
Doménique Tilleuil <domenique@tripled.io>
2022-12-01 21:10:44 +0100

    Fixed approval tests

--- src/test/java/gildedrose/ApprovalTest.java
+++ src/test/java/gildedrose/ApprovalTest.java
@@ -1,15 +1,9 @@
 package gildedrose;
 
 import org.approvaltests.combinations.CombinationApprovals;
-import org.approvaltests.reporters.DiffReporter;
-import org.approvaltests.reporters.UseReporter;
-import org.approvaltests.reporters.macosx.KaleidoscopeDiffReporter;
 import org.junit.jupiter.api.Test;
 
-import javax.swing.*;
 
-
-@UseReporter(KaleidoscopeDiffReporter.class)
 class ApprovalTest {
 
     @Test
